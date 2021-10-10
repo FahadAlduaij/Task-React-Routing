6a03b21626c5674693bcf56ce9302faccf0f7e80
fahadalduaij <fahad_osama@hotmail.com>
2021-10-10 18:12:08 +0300

    Fixing onclick image isssue

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,15 +1,11 @@
-
-
-const ProductItem = (props) => {
-  const product = props.product;
-
-  return (
-    <div className="productWrapper" onClick={() => props.setCookie(product)}>
-      <img alt={product.name} src={product.image} />
-      <p>{product.name}</p>
-      <p className="product-price">{product.price} KD</p>
-    </div>
-  );
+const ProductItem = ({ product, setCookie }) => {
+	return (
+		<div className="productWrapper" onClick={() => setCookie(product.image)}>
+			<img alt={product.name} src={product.image} />
+			<p>{product.name}</p>
+			<p className="product-price">{product.price} KD</p>
+		</div>
+	);
 };
 
 export default ProductItem;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react";
 // Components
 import ProductItem from "./ProductItem";
 import SearchBar from "./SearchBar";
@@ -6,21 +6,28 @@ import SearchBar from "./SearchBar";
 import products from "../products";
 
 const ProductList = () => {
+	const [query, setQuery] = useState("");
+	const [cookie, setCookie] = useState(
+		products.filter((x) => x.id === 1).map((x) => x.image)
+	);
 
-  const [query, setQuery] = useState("")
+	const productList = products
+		.filter((product) =>
+			product.name.toLowerCase().includes(query.toLowerCase())
+		)
+		.map((product) => (
+			<ProductItem key={product.id} product={product} setCookie={setCookie} />
+		));
 
-  const productList = products
-    .filter(product => product.name.toLowerCase().includes(query.toLowerCase()))
-    .map((product) => (
-      <ProductItem product={product} key={product.id} />
-    ));
-
-  return (
-    <>
-      <SearchBar setQuery={setQuery} />
-      <div className="listWrapper">{productList}</div>
-    </>
-  );
+	return (
+		<>
+			<SearchBar setQuery={setQuery} />
+			<div className="listWrapper">{productList}</div>
+			<footer>
+				<img className="footer" src={cookie} />
+			</footer>
+		</>
+	);
 };
 
 export default ProductList;
