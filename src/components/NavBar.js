6ea56c2bea7939f50d36a8529d853c52ea4efd4c
fahadalduaij <fahad_osama@hotmail.com>
2021-10-10 20:12:05 +0300

    Done Part 5

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import "../App.css";
-import {Link} from 'react-router-dom'
+import {Link , Redirect} from 'react-router-dom'
 
 function NavBar() {
     return (
         <header>
+            <Link to='/'>
+            <img className='logo' src={'https://cdn-icons.flaticon.com/png/128/1330/premium/1330387.png?token=exp=1633886566~hmac=cf6bc98a04747592feb8b7ec0bf830e0'} />
+             </Link>
             <Link className="btn" to='/'> Home</Link>
             <Link className="btn" to='/ProductList'> ProductList</Link>
         </header>
